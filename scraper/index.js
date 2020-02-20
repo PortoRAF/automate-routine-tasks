9c6a91991ce87f0ed1ece6012d0332d124c045ca
Renato Porto <renato@DESKTOP-RENATO.localdomain>
2020-02-19 22:23:06 -0300

    Added search in HTML for date and text values

--- scraper/index.js
+++ scraper/index.js
@@ -1,13 +1,41 @@
 const websiteUrl = "http://www.novalima.mg.gov.br/portal-transparencia/editais";
+const findClass  = ".file-list";
+const findText   = ".file-description";
+const findDate   = ".file-date";
 
-const axios = require("axios");
+const axios   = require("axios");
 const cheerio = require("cheerio");
 
 /* Use axios to obtain the page in HTML format */
 axios.get(websiteUrl)
-  .then( (response) => {
-    console.log(response.data);
+  .then((response) => {
+
+    /* Load HTML info into cheerio instance */
+    const $ = cheerio.load(response.data);
+
+    /* For each element on specified class */
+    $(findClass).each((i, elem) => {
+      /* Obtain its date and convert to text. Trim white spaces*/
+      const urlDate = $(elem).find(findDate).text().trim();
+
+      /* Obtain element text if date matches as yesterday */
+      if (urlDate == yesterday()) {
+        const urlText = $(elem).find(findText).text().trim();
+        console.log(urlText);
+        console.log(urlDate);
+      }
+    })
   })
   .catch( (error) => {
     console.log(error);
-  })
+  });
+
+  function yesterday () {
+    var yesterday = new Date();
+    yesterday.setDate(yesterday.getDate()-1);
+    var dd = String(yesterday.getDate()).padStart(2, '0');
+    var mm = String(yesterday.getMonth() + 1).padStart(2, '0');
+    var yyyy = yesterday.getFullYear();
+
+    return [dd, mm, yyyy].join('/');
+  }
