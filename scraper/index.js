8b2069acfea76735d3cbbb3d1acbc216e6e90a02
Renato Porto <renato@DESKTOP-RENATO.localdomain>
2020-02-19 23:04:31 -0300

    Added recursion to iterate over next pages

--- scraper/index.js
+++ scraper/index.js
@@ -6,36 +6,50 @@ const findDate   = ".file-date";
 const axios   = require("axios");
 const cheerio = require("cheerio");
 
-/* Use axios to obtain the page in HTML format */
-axios.get(websiteUrl)
-  .then((response) => {
+getHtml(websiteUrl, 1);
 
-    /* Load HTML info into cheerio instance */
-    const $ = cheerio.load(response.data);
+function getHtml (url, page) {
+  /* Use axios to obtain the page in HTML format */
+  axios.get(url)
+    .then((response) => {
 
-    /* For each element on specified class */
-    $(findClass).each((i, elem) => {
-      /* Obtain its date and convert to text. Trim white spaces*/
-      const urlDate = $(elem).find(findDate).text().trim();
+      /* Load HTML info into cheerio instance */
+      const $ = cheerio.load(response.data);
+      const numOfElem = 3;//$(findClass).length;
+      var displayed = 0;
 
-      /* Obtain element text if date matches as yesterday */
-      if (urlDate == yesterday()) {
-        const urlText = $(elem).find(findText).text().trim();
-        console.log(urlText);
-        console.log(urlDate);
+      /* For each element on specified class */
+      $(findClass).each((i, elem) => {
+        /* Obtain its date and convert to text. Trim white spaces*/
+        const urlDate = $(elem).find(findDate).text().trim();
+
+        /* Obtain element text if date matches as yesterday */
+        if (urlDate == yesterday()) {
+          const urlText = $(elem).find(findText).text().trim();
+          console.log(urlText);
+          console.log(urlDate);
+          displayed++;
+        }
+      })
+
+      /* Search next page in case all elements were captured */
+      if (displayed == numOfElem) {
+        var newPage = page + 1;
+        getHtml(websiteUrl + '?p=' + newPage, newPage);
       }
+
     })
-  })
-  .catch( (error) => {
-    console.log(error);
-  });
+    .catch( (error) => {
+      console.log(error);
+    });
+};
 
-  function yesterday () {
-    var yesterday = new Date();
-    yesterday.setDate(yesterday.getDate()-1);
-    var dd = String(yesterday.getDate()).padStart(2, '0');
-    var mm = String(yesterday.getMonth() + 1).padStart(2, '0');
-    var yyyy = yesterday.getFullYear();
+function yesterday () {
+  var yesterday = new Date();
+  yesterday.setDate(yesterday.getDate()-1);
+  var dd = String(yesterday.getDate()).padStart(2, '0');
+  var mm = String(yesterday.getMonth() + 1).padStart(2, '0'); // January is 0!
+  var yyyy = yesterday.getFullYear();
 
-    return [dd, mm, yyyy].join('/');
-  }
+  return [dd, mm, yyyy].join('/');
+};
